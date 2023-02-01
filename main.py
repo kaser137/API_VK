786732ba06e014b4832b22a20751f97dd7fe3355
kaser137 <107580630+kaser137@users.noreply.github.com>
2023-02-01 23:36:08 +0200

    Remove pipeline.

--- main.py
+++ main.py
@@ -1,14 +1,21 @@
 import dotenv
 import os
+import random
 from pathlib import Path
-from servicefile import publish_wall_image
+from servicefile import save_wall_image, get_wall_upload_url, fetch_comic, get_latest_comic_num, get_comic_url_comments, publish_wall_image
 
 
 def main():
     dotenv.load_dotenv(Path('venv', '.env'))
     token = os.environ['VK_TOKEN']
     group_id = int(os.environ['VK_GROUP_ID'])
-    print(publish_wall_image(token, group_id))
+    random_comic_num = random.randint(1, get_latest_comic_num()+1)
+    url, comments = get_comic_url_comments(random_comic_num)
+    image = fetch_comic(url)
+    upload_url = get_wall_upload_url(token, group_id)
+    photo_id = save_wall_image(token, group_id, upload_url, image)
+    os.remove(image)
+    print(publish_wall_image(token, group_id, photo_id, comments))
 
 
 if __name__ == '__main__':
--- servicefile.py
+++ servicefile.py
@@ -1,74 +1,58 @@
-import random
-import time
 import requests
-import os
 from pathlib import Path
 
 
-def request_api(url, payload=None, attempt_timeout=10):
-    flag = True
-    while flag:
-        try:
-            response = requests.get(url, params=payload)
-            response.raise_for_status()
-            flag = False
-            return response
-        except requests.exceptions.ConnectionError or requests.exceptions.Timeout:
-            print(f'connection failed, next attempt in {attempt_timeout} seconds')
-            time.sleep(attempt_timeout)
+def get_latest_comic_num():
+    response = requests.get('https://xkcd.com/info.0.json')
+    response.raise_for_status()
+    return response.json()['num']
 
 
-def grab_img(url, name_for_img, payload=None):
-    response = request_api(url, payload)
-    with open(name_for_img, 'wb') as file:
+def get_comic_url_comments(comic_num):
+    response = requests.get(f'https://xkcd.com/{comic_num}/info.0.json')
+    response.raise_for_status()
+    response = response.json()
+    return response['img'], response['alt']
+
+
+def fetch_comic(url, payload=None):
+    response = requests.get(url, payload)
+    response.raise_for_status()
+    name_for_comic = Path(url).parts[-1]
+    with open(name_for_comic, 'wb') as file:
         file.write(response.content)
-
-
-def get_latest_comic():
-    return request_api('https://xkcd.com/info.0.json').json()['num']
-
-
-def get_random_comic():
-    random_comic = random.randint(1, get_latest_comic() + 1)
-    response = request_api(f'https://xkcd.com/{random_comic}/info.0.json').json()
-    grab_img(response['img'], Path(response['img']).parts[-1])
-    return Path(response['img']).parts[-1], response['alt']
-
+    return name_for_comic
 
 def get_wall_upload_url(token, group_id, v=5.131):
     payload = {'v': v, 'access_token': token, 'group_id': group_id}
-    response = request_api('https://api.vk.com/method/photos.getWallUploadServer', payload)
+    response = requests.get('https://api.vk.com/method/photos.getWallUploadServer', params=payload)
     upload_url = response.json()['response']['upload_url']
     return upload_url
 
 
-def transfer_image(token, group_id, image):
-    upload_url = get_wall_upload_url(token, group_id)
+def transfer_image(upload_url, image):
     with open(image, 'rb') as img:
         files = {'photo': img}
         response = requests.post(upload_url, files=files)
-        response.raise_for_status()
-        answer = response.json()
-    return answer['server'], answer['photo'], answer['hash']
+    response.raise_for_status()
+    response = response.json()
+    return response['server'], response['photo'], response['hash']
 
 
-def save_wall_image(token, group_id, image, v=5.131):
-    img_server, img_photo, img_hash = transfer_image(token, group_id, image)
+def save_wall_image(token, group_id, upload_url, image, v=5.131):
+    img_server, img_photo, img_hash = transfer_image(upload_url, image)
     payload = {'server': img_server, 'photo': img_photo, 'hash': img_hash, 'v': v, 'access_token': token,
                'group_id': group_id}
     response = requests.post('https://api.vk.com/method/photos.saveWallPhoto', params=payload)
     response.raise_for_status()
     answer = response.json()['response'][0]
-    photo = ''.join(('photo', str(answer['owner_id']), '_', str(answer['id'])))
-    return photo
+    photo_id = ''.join(('photo', str(answer['owner_id']), '_', str(answer['id'])))
+    return photo_id
 
 
-def publish_wall_image(token, group_id, v=5.131):
-    image, comments = get_random_comic()
-    photo = save_wall_image(token, group_id, image)
-    payload = {'v': v, 'access_token': token, 'attachments': photo, 'from_group': 1, 'owner_id': -group_id,
+def publish_wall_image(token, group_id, photo_id, comments, v=5.131):
+    payload = {'v': v, 'access_token': token, 'attachments': photo_id, 'from_group': 1, 'owner_id': -group_id,
                'message': comments}
     response = requests.post('https://api.vk.com/method/wall.post', params=payload)
     response.raise_for_status()
-    os.remove(image)
     return response.json()
