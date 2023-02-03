6e76f5d5e7366955b160658dfe0eeeec45e3fc87
kaser137 <107580630+kaser137@users.noreply.github.com>
2023-02-03 05:40:51 +0200

    Reformat main.py.

--- main.py
+++ main.py
@@ -2,8 +2,12 @@ import dotenv
 import os
 import random
 from pathlib import Path
-from servicefile import save_wall_image, get_wall_upload_url, fetch_comic, get_latest_comic_num, \
-    get_comic_url_and_comments, publish_wall_image
+from servicefile import save_wall_image
+from servicefile import get_wall_upload_url
+from servicefile import fetch_comic
+from servicefile import get_latest_comic_num
+from servicefile import get_comic_url_and_comments
+from servicefile import publish_wall_image
 
 
 def main():
@@ -13,12 +17,12 @@ def main():
         group_id = int(os.environ['VK_GROUP_ID'])
         random_comic_num = random.randint(1, get_latest_comic_num()+1)
         url, comments = get_comic_url_and_comments(random_comic_num)
-        image_path = fetch_comic(url)
+        image_name = fetch_comic(url)
         upload_url = get_wall_upload_url(token, group_id)
-        photo_id = save_wall_image(token, group_id, upload_url, image_path)
+        photo_id = save_wall_image(token, group_id, upload_url, image_name)
         print(publish_wall_image(token, group_id, photo_id, comments))
     finally:
-        os.remove(image_path)
+        os.remove(image_name)
 
 
 if __name__ == '__main__':
