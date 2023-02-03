2c9c0576d82a18b219c3cbf6e3764d184f1f1a1b
kaser137 <107580630+kaser137@users.noreply.github.com>
2023-02-03 04:02:25 +0200

    Change names.md.

--- servicefile.py
+++ servicefile.py
@@ -31,7 +31,7 @@ def get_wall_upload_url(token, group_id, v=5.131):
     return upload_url
 
 
-def upload_to_vk_image(upload_url, image):
+def upload_image_to_vk(upload_url, image):
     with open(image, 'rb') as img:
         files = {'photo': img}
         response = requests.post(upload_url, files=files)
@@ -41,7 +41,7 @@ def upload_to_vk_image(upload_url, image):
 
 
 def save_wall_image(token, group_id, upload_url, image, v=5.131):
-    img_server, img_photo, img_hash = upload_to_vk_image(upload_url, image)
+    img_server, img_photo, img_hash = upload_image_to_vk(upload_url, image)
     payload = {'server': img_server, 'photo': img_photo, 'hash': img_hash, 'v': v, 'access_token': token,
                'group_id': group_id}
     response = requests.post('https://api.vk.com/method/photos.saveWallPhoto', params=payload)
